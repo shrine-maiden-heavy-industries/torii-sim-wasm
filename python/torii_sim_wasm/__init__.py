a2aaf67a58513cae257cd5a324c85419dc707c89
Miika Alikirri <duck@duckie.fi>
2025-09-05 23:14:42 +0300

    python: add timeline to the simulation state

--- python/torii_sim_wasm/__init__.py
+++ python/torii_sim_wasm/__init__.py
@@ -157,8 +157,59 @@ class _VCDWriter:
 		if self._close_gtkw:
 			self.gtkw_file.close()
 
+class _Timeline:
+	def __init__(self) -> None:
+		self.now = 0
+		self.deadlines = dict()
+
+	def reset(self):
+		self.now = 0
+		self.deadlines.clear()
+
+	def at(self, run_at, process):
+		if process in self.deadlines:
+			raise ValueError(f'Process {process!r} already in deadline list')
+		self.deadlines[process] = run_at
+
+	def delay(self, delay_by, process):
+		if delay_by is None:
+			run_at = self.now
+		else:
+			run_at = self.now + delay_by
+		self.at(run_at, process)
+
+	def advance(self):
+		nearest_processes = set()
+		nearest_deadline = None
+		for process, deadline in self.deadlines.items():
+			if deadline is None:
+				if nearest_deadline is not None:
+					nearest_processes.clear()
+				nearest_processes.add(process)
+				nearest_deadline = self.now
+				break
+			elif nearest_deadline is None or deadline <= nearest_deadline:
+				if deadline < self.now:
+					raise ValueError(f'Deadline of {deadline} is behind our current state of {self.now}')
+
+				if nearest_deadline is not None and deadline < nearest_deadline:
+					nearest_processes.clear()
+				nearest_processes.add(process)
+				nearest_deadline = deadline
+
+		if not nearest_processes:
+			return False
+
+		for process in nearest_processes:
+			process.runnable = True
+			del self.deadlines[process]
+		self.now = nearest_deadline
+
+		return True
+
 class _WASMimulation(BaseSimulation):
 	def __init__(self) -> None:
+		self.timeline = _Timeline()
 		self.signals  = SignalDict()
 		self.slots    = []
 		self.pending  = set()
@@ -174,6 +225,7 @@ class _WASMimulation(BaseSimulation):
 class WASMSimEngine(BaseEngine):
 	def __init__(self, fragment: Fragment) -> None:
 		self._state = _WASMimulation()
+		self._timeline = self._state.timeline
 		self._frag = fragment
 		self._processes = WASMFragmentCompiler(self._state)(self._frag)
 		self._vcd_writers = []
@@ -192,7 +244,7 @@ class WASMSimEngine(BaseEngine):
 
 	@property
 	def now(self):
-		return 0
+		return self._timeline.now
 
 	@contextmanager
 	def write_vcd(
