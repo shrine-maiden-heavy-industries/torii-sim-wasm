505432b024aab939dc8604d4daa10f8d772a1d12
Miika Alikirri <duck@duckie.fi>
2025-09-05 22:44:18 +0300

    python: Add wasm compiler stubs

--- python/torii_sim_wasm/wasmrtl.py
+++ python/torii_sim_wasm/wasmrtl.py
@@ -1,14 +1,77 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
-from torii.hdl.ir import Fragment
+from torii.hdl.ast   import SignalSet
+from torii.hdl.ir    import Fragment
+from torii.hdl.xfrm  import LHSGroupFilter, StatementVisitor, ValueVisitor
+from torii.sim._base import BaseProcess
 
 __all__ = (
 	'WASMFragmentCompiler',
+	'WASMRTLProcess',
 )
+
+class WASMRTLProcess(BaseProcess):
+	__slots__ = ('is_comb', 'runnable', 'passive', 'run')
+
+	def __init__(self, *, is_comb) -> None:
+		self.is_comb  = is_comb
+
+		self.reset()
+
+	def reset(self):
+		self.runnable = self.is_comb
+		self.passive  = True
+
+class _Compiler:
+	def __init__(self, state, emitter) -> None:
+		self.state = state
+		self.emitter = emitter
+
+class _ValueCompiler(ValueVisitor, _Compiler):
+	...
+
+class _RHSValueCompiler(_ValueCompiler):
+	def __init__(self, state, emitter, *, mode, inputs = None) -> None:
+		super().__init__(state, emitter)
+		if mode not in ('curr', 'next'):
+			raise ValueError(f'Expected mode to be \'curr\', or \'next\', not \'{mode!r}\'')
+		self.mode = mode
+		# If not None, `inputs` gets populated with RHS signals.
+		self.inputs = inputs
+
+class _LHSValueCompiler(_ValueCompiler):
+	def __init__(self, state, emitter, *, rhs, outputs = None) -> None:
+		super().__init__(state, emitter)
+		# `rrhs` is used to translate rvalues that are syntactically a part of an lvalue, e.g.
+		# the offset of a Part.
+		self.rrhs = rhs
+		# `lrhs` is used to translate the read part of a read-modify-write cycle during partial
+		# update of an lvalue.
+		self.lrhs = _RHSValueCompiler(state, emitter, mode = 'next', inputs = None)
+		# If not None, `outputs` gets populated with signals on LHS.
+		self.outputs = outputs
+
+class _StatementCompiler(StatementVisitor, _Compiler):
+	def __init__(self, state, emitter, *, inputs = None, outputs = None) -> None:
+		super().__init__(state, emitter)
+		self.rhs = _RHSValueCompiler(state, emitter, mode = 'curr', inputs = inputs)
+		self.lhs = _LHSValueCompiler(state, emitter, rhs = self.rhs, outputs = outputs)
 
 class WASMFragmentCompiler:
 	def __init__(self, state) -> None:
 		self.state = state
 
 	def __call__(self, fragment: Fragment):
-		pass
+		processes = set()
+
+		for domain_name, domain_signals in fragment.drivers.items():
+			domain_stmts = LHSGroupFilter(domain_signals)(fragment.statements)
+			domain_process = WASMRTLProcess(is_comb = domain_name is None)
+
+			if domain_name is None:
+				inputs = SignalSet()
+				_StatementCompiler(self.state, None, inputs = inputs)(domain_stmts)
+			else:
+				_StatementCompiler(self.state, None)(domain_stmts)
+
+			processes.add(domain_process)
