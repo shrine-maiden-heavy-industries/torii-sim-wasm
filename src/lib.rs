2a3116428dba396559793cb3a05ca26868c5401c
Miika Alikirri <duck@duckie.fi>
2025-09-06 16:20:11 +0300

    rust: add WASMInstance and WASMValue python objects

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,19 @@
 
 use pyo3::prelude::*;
 
+mod memory;
+
 #[pymodule]
 #[pyo3(name = "_wasm_engine")]
 mod wasm_engine {
-    use pyo3::prelude::*;
+    use crate::memory;
+    use pyo3::{prelude::*, py_run};
 
     #[pymodule_init]
     fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
         m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-
+        m.add_class::<memory::WASMValue>()?;
+        m.add_class::<memory::WASMInstance>()?;
         Ok(())
     }
 }
---
+++ src/memory.rs
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: BSD-2-Clause
+
+use pyo3::prelude::*;
+use wasmtime::{Config, Engine, Memory, MemoryTypeBuilder, Store};
+
+#[pyclass]
+pub struct WASMInstance {
+    memory: Memory,
+    store: Store<()>,
+}
+
+#[pymethods]
+impl WASMInstance {
+    #[new]
+    pub fn new() -> Self {
+        let mut config = Config::new();
+        config.strategy(wasmtime::Strategy::Winch);
+        let engine = Engine::new(&config).unwrap();
+        let mut store = Store::new(&engine, ());
+
+        let mem_type = MemoryTypeBuilder::new()
+            .memory64(true)
+            .shared(true)
+            .min(2)
+            .max(Some(2))
+            .build()
+            .unwrap();
+        let memory = Memory::new(&mut store, mem_type).unwrap();
+        Self { memory, store }
+    }
+}
+
+#[pyclass]
+pub struct WASMValue {
+    /// Pointer to the wasm value, unsafely as usize as *mut u8 makes pyo3 unhappy
+    ptr: usize,
+    /// Value length in bits
+    length: u64,
+}
+
+#[pymethods]
+impl WASMValue {
+    #[new]
+    pub fn new(instance: &WASMInstance, length: u64, offset: usize, value: u64) -> Self {
+        let ptr = unsafe { instance.memory.data_ptr(&instance.store).add(offset * 8) as usize };
+        let new = Self { ptr, length };
+        new.set(value);
+        new
+    }
+
+    pub fn set(&self, value: u64) {
+        // make sure the value is always fits the bits
+        let value = value & ((1 << self.length) - 1);
+        // TODO: fix for big as wasm values are always little endian
+        unsafe { *(self.ptr as *mut u64) = value };
+    }
+
+    pub fn get(&self) -> u64 {
+        // TODO: fix for big endian as value here is always little endian
+        unsafe { *(self.ptr as *mut u64) }
+    }
+}
