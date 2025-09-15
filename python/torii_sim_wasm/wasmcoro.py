09a73572000346a7ca65e70329a45ba903f20447
Miika Alikirri <duck@duckie.fi>
2025-09-15 12:34:57 +0300

    python: Don't recompile commands in wasmcoro
    Each time wasmcoro.run is called and command instance is Value or
    Statement, a new wasm module is generated and compiled. Everytime a new
    wasm module is created, a new wasm instance is created too. Wasmtime
    limits these instances to 10000 so recompiling existing modules should
    be avoided when ever possible.
    
    Fixes: https://github.com/shrine-maiden-heavy-industries/torii-sim-wasm/issues/2

--- python/torii_sim_wasm/wasmcoro.py
+++ python/torii_sim_wasm/wasmcoro.py
@@ -81,15 +81,18 @@ class WASMCoroProcess(BaseProcess):
 				if isinstance(command, ValueCastable):
 					command = Value.cast(command)
 				if isinstance(command, Value):
-					module_code = _RHSValueCompiler.compile(self.state, command, mode = 'curr')
-					run = WASMRunner(module_code, self.state.memory, self.state.set_slot)
-					result = run()
+					if not hasattr(command, 'runner'):
+						module_code = _RHSValueCompiler.compile(self.state, command, mode = 'curr')
+						command.runner = WASMRunner(module_code, self.state.memory, self.state.set_slot)
+
+					result = command.runner()
 					response = Const.normalize(result, command.shape())
 
 				elif isinstance(command, Statement):
-					module_code = _StatementCompiler.compile(self.state, command)
-					run = WASMRunner(module_code, self.state.memory, self.state.set_slot)
-					run()
+					if not hasattr(command, 'runner'):
+						module_code = _StatementCompiler.compile(self.state, command)
+						command.runner = WASMRunner(module_code, self.state.memory, self.state.set_slot)
+					command.runner()
 
 				elif type(command) is Tick:
 					domain = command.domain
