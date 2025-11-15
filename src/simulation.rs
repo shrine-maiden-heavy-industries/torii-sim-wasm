77a4e3c20a5a1df6807780be52afcfb4952b3be2
Miika Alikirri <duck@duckie.fi>
2025-11-15 15:33:46 +0200

    rust: add python trigger to state waiters

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,4 +1,8 @@
-use pyo3::{prelude::*, types::PyDict};
+use pyo3::{
+    exceptions::PyValueError,
+    prelude::*,
+    types::{PyDict, PyNone},
+};
 
 use crate::memory::{WASMInstance, WASMValue};
 
@@ -8,19 +12,20 @@ pub struct WASMSignalState {
     pub signal_size: u64,
     pub curr: WASMValue,
     pub next: WASMValue,
+    pub waiters: Py<PyDict>,
     // TODO: pending
-    // TODO: waiters
 }
 
 #[pymethods]
 impl WASMSignalState {
     #[new]
     fn new(instance: &WASMInstance, index: usize, signal_size: u64, value: u64) -> Self {
-        Self {
+        Python::attach(|py| Self {
             signal_size,
+            waiters: PyDict::new(py).into(),
             curr: WASMValue::new(instance, signal_size, index * 2, value),
             next: WASMValue::new(instance, signal_size, index * 2 + 1, value),
-        }
+        })
     }
 }
 
@@ -76,8 +81,35 @@ impl WASMSimulation {
         Ok(())
     }
 
-    fn add_trigger(&mut self, _process: Py<PyAny>, _signal: Py<PyAny>) -> PyResult<()> {
-        Ok(())
+    #[pyo3(signature = (process, signal, *, trigger = None))]
+    fn add_trigger(
+        &mut self,
+        process: Py<PyAny>,
+        signal: Py<PyAny>,
+        trigger: Option<Py<PyAny>>,
+    ) -> PyResult<()> {
+        let index = self.get_signal(signal)?;
+        Python::attach(|py| {
+            // we need to convert the option to python None
+            let trigger = if let Some(trigger) = trigger {
+                trigger
+            } else {
+                PyNone::get(py).as_any().clone().unbind()
+            };
+
+            let waiters = self.slots[index].waiters.bind(py);
+            if waiters
+                .get_item(process.clone_ref(py))?
+                .is_some_and(|value| value.eq(trigger.clone_ref(py)).unwrap())
+            {
+                return Err(PyErr::new::<PyValueError, _>(
+                    "Unable to add trigger for process!",
+                ));
+            }
+
+            waiters.set_item(process, trigger.clone_ref(py))?;
+            Ok(())
+        })
     }
 
     fn set_slot(&mut self) -> PyResult<()> {
